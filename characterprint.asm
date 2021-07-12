d3f3c4f1062fc934fb3344a9f14d80bc5b0b3cf6
wrtnspknbrkn <55252590+Joseph-DKE@users.noreply.github.com>
2021-07-12 07:29:44 +0000

    adding comments

--- characterprint.asm
+++ characterprint.asm
@@ -19,76 +19,82 @@ section .text
 	global _start
 
 _start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, name
-	mov rdx, 26
-	syscall
+	mov rax, 1          ;sys_write
+	mov rdi, 1          ;standard output
+	mov rsi, name       ;text to print
+	mov rdx, 26         ;length
+	syscall             ;kernel call
 
-	mov rbx, name
-	mov rdi, 0
+	mov rbx, name       ;put name into rbx
+	mov rdi, 0          ;set counter to 0
 
-	dec rbx
+	dec rbx             ;move to before first character in rbx
 
-	call lp
+	call lp             ;call label lp
 
-	jmp exit
+	jmp exit            ;jump to exit
 
-lp:
-	inc rbx
-	mov rcx, wme
+lp:                         ;compare each character in rbx with the first character in rcx till there is a match
+	inc rbx             ;move to next character in rbx
+	mov rcx, wme        ;sset rcx to first character in wme
 
-        inc rdi
-	mov al, [rbx]
-	mov ah, [rcx]
-	cmp al, ah
-	jne lp
-	jmp lpp
+        inc rdi             ;increment counter by 1
+	mov al, [rbx]       ;move current character in rbx to al
+	mov ah, [rcx]       ;move current character in rcx to ah
+	cmp al, ah          ;compare ah and al (current character in rcx and current character in rbx)
+	jne lp              ;jump if not equal to lp label
+	jmp lpp             ;else jump to lpp label
 
-lpp:
-	inc rbx
-	inc rcx
-	inc rdi
-	mov al, [rbx]
-	mov ah, [rcx]
-	cmp al, ah
-	jne lqp
-	jmp lpp
+lpp:                        ;comparing character by character after thre is a match found in lp label
+	inc rbx             ;move to next character in rbx
+	inc rcx             ;move to next character in rcx
+	inc rdi             ;increment counter by 1
+	mov al, [rbx]       ;move ccurrent character in rbx into al
+	mov ah, [rcx]       ;move current character in rcx into ah
+	cmp al, ah          ;compare al and ah
+	jne lqp             ;jump if not equal to lqp label
+	jmp lpp             ;else jump to lpp label
 
 
-lqp:
-	cmp ah, 0
-	jne lp
-	jmp loop
+lqp:                        ;check if we are at the end of the rcx string
+	cmp ah, 0           ;compare ah to 0
+	jne lp              ;jump if not equal to lp
+	jmp loop            ;else jump to loop
 
-loop:
-		dec rbx
-		dec rcx
-		dec rdi
-		mov al, [rbx]
-		mov ah, [rcx]
-		cmp al, ah
-		je loop
+loop:                       ;setting rbx to beginning of match with rcx
+	dec rbx	 	    ;move to previous ccharacter in rbx
+	dec rcx 	    ;move to previous character in rcx
+	dec rdi 	    ;decrease counter by 1
+	mov al, [rbx]       ;move current character in rbx into al
+	mov ah, [rcx]       ;move current character in rcx into ah
+	cmp al, ah          ;compare al and ah
+	je loop             ;if equal, jump to loop
 
-		mov rbx, name
-		jmp zoqqq
-zoqqq:
-	inc rbx
-	dec rdi
-	cmp rdi, 0
-	jne zoqqq
-		lpl:
-			mov rax, 1
-			mov rdi, 1
-			mov rsi, rbx
-			mov rdx, 1
-			syscall
-			inc rbx
-			mov ah, [rbx]
-			cmp ah, 0
-			jne lpl
+	mov rbx, name       ;else move name into rbx (set rbx to first character in name)
+	jmp zoqqq           ;jump to zoqqq label
 
-		ret
+zoqqq:                      ;setting rbx to first character in match with rcx for printing (this step is not actually necessary so
+			        ;you can skip this and jump to lpl label
+	inc rbx             ;move to next character in rbx
+	dec rdi             ;decrease counter by 1
+	cmp rdi, 0          ;compare counter with 0
+	jne zoqqq           ;loop if not equal (jump to zoqqq label)
+
+	jmp lpl             ;else jump to lpl label
+	
+lpl: 		            ;print loop (printing rbx character by charactre till end of string)
+	mov rax, 1          ;sys_write
+	mov rdi, 1          ;standard output
+	mov rsi, rbx        ;text to print (string in rbx)
+	mov rdx, 1          ;length of string to print (one space for one character at a time)
+	syscall             ;kernel call
+	inc rbx             ;move to next character in rbx
+	mov ah, [rbx]       ;move current character in rbx into ah
+	cmp ah, 0           ;compare ah to 0 (end character)
+	jne lpl             ;loop if not equal (jump to lpl label)
+
+	jmp exit            ;else jump to exit
+
 exit:
-	mov rax, 60
-	syscall
+	mov rax, 60         ;sys_exit
+	syscall             ;kernel call
